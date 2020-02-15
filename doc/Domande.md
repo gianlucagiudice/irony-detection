ea892a75e4913290cd52862dd747b2e6f5657b6c
Gianluca Giudice <gianlu.giudice@gmail.com>
2020-02-15 18:59:27 +0100

    Pos tagger completed

--- doc/Domande.md
+++ doc/Domande.md
@@ -1,9 +1,13 @@
+# Domande
+
+## Tokenizer
 - Devo togliere i Retweet --> "RT @ ..."?
 - Devo togliere i tag?
 - Devo tenere i link?
 - Come tratto le forme contratte?
     - ES. Won't
         - Ora le tengo così come sono
+        - ark-tweet-npl crea due token "won", "t"
 - RT @DjBlack_Pearl: wat muhfuckaz wearin 4 the lingerie party?????
   - wearing 4. Come tratto questo caso?
 - RT @eye_ee_duh_Esq: LMBO! This man filed an EMERGENCY Motion for Continuance on account of the Rangers game tonight! « Wow lmao
@@ -19,3 +23,15 @@
         - In questo modo tengo le forme contratte
 - E' un problema se ad ogni esecuzione dello script, ho una permutazione su T = {t_1, t_2, ...t_n}, dove T è la colonna della matrice
 - Ho fatto il tokenizer ma ho visto che è già implementato nello script. Quale devo usare?
+
+## Pos tag
+- Devo taggare le words sul mio tokenizer?
+- Se costruisco prima la matrice e poi guardo i tag assegnati, nel caso di 2 occerrenze in un tweet perdo iniettività
+- POS tagger script ---> BUG?
+
+#### BUG pos tagger?
+Problema parol
+
+》have
+
+è considerata parola
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from Tokenizer import Tokenizer
-from TextFeature import TextFeature
+import subprocess
+
+from language.Tokenizer import Tokenizer
+from language.TextFeature import TextFeature
+from language.PosFeature import PosFeature
 
 # Set tweets path
-tweetsPath = "examples/example_tweets_small.txt"
+tweetsPath = "tests/example_tweets_small.txt"
 # Parameters
 debug = True
 
 
 def main():
+    # Tokenizer process
+    print("Tokenizer . . .")
     # Create tokenizer object
     toknz = Tokenizer(tweetsPath)
     # Compute all tweets
@@ -17,12 +22,23 @@ def main():
     # Get list of words
     words_list = toknz.words
 
+    # Evaluate text feature
+    print("Text feature . . .")
     # Crete text feature object
     txtf = TextFeature(words_list)
     # Extract matrix from words list
     txtf.extractMatrix()
     # Print results
     print(txtf if debug else "", end='')
+
+    # Words tagging
+    print("Pos tagging . . .")
+    # Tag tweets
+    pos = PosFeature(toknz.words, toknz.tweets)
+    # Tag part of speech
+    pos.computePosTag()
+    # Print results
+    print(pos if debug else "", end='')
 
 
 if __name__ == '__main__':
