c0153bd169983b271cf9bd4e1b3aae289c4c767f
Gianluca Giudice <gianlu.giudice@gmail.com>
2020-02-27 02:13:14 +0100

    Clean Code

--- language/Debugger.py
+++ language/Debugger.py
@@ -1,14 +1,13 @@
 class Debugger:
 
     def __str__(self, limit=-1, *args, **kwargs):
-        sep_char = "-"*5
-        str_obj = "%%%% {} %%%%\n".format(kwargs['title'].upper())
-        a = list(args)
-        for index, *debug in enumerate(zip(*args)):
-            p = list(*debug)
+        sep_char = "-" * 4
+        title_char = "%" * 5
+        str_obj = "{} {} {}\n".format(title_char, kwargs['title'].upper(), title_char)
+        v = enumerate(zip(*args))
+        for index, *debug, in enumerate(zip(*args)):
             if index == limit:
                 break
-            header = "{} {} N. {} {}".format(sep_char, kwargs['header'], index, sep_char)
-            x = kwargs['template'].format(*p)
-            str_obj += "{}\n{}\n".format(header, x)
+            header = "{} {} N. {} {}".format(sep_char, kwargs['header'], index + 1, sep_char)
+            str_obj += "{}\n{}\n".format(header, kwargs['template'].format(*debug[0]))
         return "{}\n".format(str_obj)
