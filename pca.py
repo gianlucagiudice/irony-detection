3598ad2f6e316c005533ebaeeda42ff5f697385b
gianluca <gianlu.giudice@gmail.com>
2020-05-19 14:52:00 +0200

    PCA transformation

--- pca.py
+++ pca.py
@@ -1,13 +1,13 @@
-import csv
-
 import numpy as np
+import pandas as pd
 import torch
 from pytorch_pretrained_bert import BertTokenizer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-from src.dataset.DataFrame import create_folder
 from src.dataset.Dataset import Dataset
 from src.features.text.Bert import Bert
-from src.utils.config import DATASET_PATH_OUT
+from src.utils.config import REPORTS_PATH
 from src.utils.parameters import TARGET_DATASET
 
 
@@ -22,15 +22,30 @@ class Pca:
         self.model = Bert.build_model()
         # Matrix dict
         self.average_dict = dict()
+        # Matrix
+        self.matrix = None
+        # Idx
+        self.idx = None
+        # Principal component
+        self.principal_components = None
+        # Out dataframe
+        self.df = None
 
     def compute_matrix(self):
         # Compute dict
         self.compute_dict()
         # Write dict to file
-        self.dump_dict()
+        # Create matrix
+        self.fill_matrix()
+        # Fit pca
+        self.transform()
+        # Create dataframe
+        self.build_df()
+        # Save dataframe
+        self.dump_df()
 
     def compute_dict(self):
-        for tweet in self.tweet_list:
+        for i, tweet in enumerate(self.tweet_list):
             # Tokenize tweet
             indexed_tokens, segments_ids = Bert.tokenize(tweet, self.tokenizer)
             # Create tensor
@@ -41,21 +56,43 @@ class Pca:
                 encoded_layers, _ = self.model(tokens_tensor, segments_tensor)
             token_embeddings = Pca.reshape_layers(encoded_layers)
             # Update dict
+            print("\r\t{}% completed".format(0), end='')
             for idx, token in zip(indexed_tokens, token_embeddings):
                 cat_token = np.array(Pca.pooling_strategy(token))
                 to_average = self.average_dict.get(idx, cat_token)
                 self.average_dict[idx] = np.mean([cat_token, to_average], axis=0)
+            if i % 100:
+                perc = round(i / len(self.tweet_list) * 100, 3)
+                print("\r\t{}% completed".format(perc), end='')
+        print("\r\t{}% completed".format(100))
 
-    def dump_dict(self):
-        path = '{}{}.pca/'.format(DATASET_PATH_OUT, TARGET_DATASET)
-        # Create folder for target path
-        create_folder('{}.pca'.format(TARGET_DATASET))
-        file_name = '{}{}.csv'.format(path, 'PCA')
-        # Write to file
-        with open(file_name, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            [writer.writerow(list(value) + [idx]) for idx, value in self.average_dict.items()]
+    def fill_matrix(self):
+        # Build matrix
+        n_row = len(self.average_dict)
+        n_cols = len(next(iter(self.average_dict.values())))
+        self.matrix = np.zeros((n_row, n_cols))
+        self.idx = np.zeros((n_row, 1))
+        # Fill matrix
+        for i, (idx, value) in enumerate(self.average_dict.items()):
+            self.matrix[i] = value
+            self.idx[i] = idx
+        # Standardize the Data
+        self.matrix = StandardScaler().fit_transform(self.matrix)
 
+    def transform(self):
+        # Transform data
+        pca = PCA(n_components=2)
+        self.principal_components = pca.fit_transform(self.matrix)
+
+    def build_df(self):
+        data = np.concatenate([self.principal_components, self.idx], axis=1)
+        # Create dataframe
+        self.df = pd.DataFrame(
+            data=data, columns=['principal component 1', 'principal component 2', 'idx'])
+
+    def dump_df(self):
+        filename = '{}{}.pca/PCA.pkl'.format(REPORTS_PATH, TARGET_DATASET)
+        self.df.to_pickle(filename)
 
     @staticmethod
     def reshape_layers(encoded_layers):
@@ -72,17 +109,21 @@ class Pca:
     def pooling_strategy(token):
         return torch.cat((token[-4],token[-3],token[-2],token[-1]))
 
+
 def main():
     # Read all tweets
-    print("Reading dataset . . .")
+    print("> Reading dataset . . .")
     tweets = Dataset(TARGET_DATASET).extract()
-    print("Reading completed.")
-    # PCA
-    print("Computing matrix . . .")
+    print("\tReading completed.")
+    # PCA matrix
+    print("> Computing matrix . . .")
     pca = Pca(tweets)
     pca.compute_matrix()
-    print("Computing completed")
+    print("\tComputing completed")
+    # PCA fit
+    print("> Transforming data . . .")
+    pca.transform()
+    print("\tTransformation completed . . .")
 
-    x = 0
 
 main()
